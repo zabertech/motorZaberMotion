8e226d7ba0841cc2d4a899e30af14761bb19b6d1
colbysparks <colby.sparks@zaber.com>
2024-11-29 14:49:03 -0800

    Update Support package

--- zaberMotionSupport/ZaberMotionCppSupport/include/zaber/motion/version.h
+++ zaberMotionSupport/ZaberMotionCppSupport/include/zaber/motion/version.h
@@ -2,5 +2,5 @@
 
 #define ZML_VERSION_MAJOR 7
 #define ZML_VERSION_MINOR 2
-#define ZML_VERSION_PATCH 0
-#define ZML_VERSION_STR "7.2.0"
+#define ZML_VERSION_PATCH 1
+#define ZML_VERSION_STR "7.2.1"
